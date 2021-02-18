8486c0bfc0fb37c4fe335d50761ec10d0369457d
Kishan Kr Sharma <skishan781@gmail.com>
2021-02-18 12:59:34 +0530

    UI completed

--- app/src/main/java/com/heycode/mlappthree/MainActivity.kt
+++ app/src/main/java/com/heycode/mlappthree/MainActivity.kt
@@ -2,10 +2,23 @@ package com.heycode.mlappthree
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.nex3z.fingerpaintview.FingerPaintView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var clear: Button
+    private lateinit var predict: Button
+    private lateinit var draw_area: FingerPaintView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        clear = findViewById(R.id.clear_value_button)
+        predict = findViewById(R.id.predict_button)
+        draw_area = findViewById(R.id.draw_area)
+
+        clear.setOnClickListener {
+            draw_area.clear()
+        }
     }
 }
